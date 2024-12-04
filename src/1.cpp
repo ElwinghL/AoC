e336d5dbad129c0ae5a6f92268e23e5123273a03
ElwinghL <clement_1996@hotmail.com>
2024-12-04 17:29:51 +0100

    Ajout de la solution pour le jour 1

---
+++ src/1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <sstream>
+#include <algorithm> // std::sort
+#include <vector>
+
+using namespace std;
+
+void splitLine(string line, string *s1, string *s2)
+{
+    istringstream sline(line);
+    getline(sline, *s1, ' ');
+    for (int i = 0; i <= 3; ++i)
+    {
+        getline(sline, *s2, ' ');
+    }
+}
+
+void getInts(string line, long int *d1, long int *d2)
+{
+    string s1 = "", s2 = "";
+    splitLine(line, &s1, &s2);
+    *d1 = stol(s1.c_str());
+    *d2 = stol(s2.c_str());
+}
+
+void fillTabs(string line, vector<long int> *tab1, vector<long int> *tab2)
+{
+    long int d1 = 0, d2 = 0;
+    getInts(line, &d1, &d2);
+
+    tab1->push_back(d1);
+    tab2->push_back(d2);
+}
+
+bool lSort(long int a, long int b) { return a < b; }
+
+int main()
+{
+    ifstream file;
+    string line;
+    file.open("./res/1.res");
+
+    long int result = 0;
+    vector<long int> tab1, tab2;
+
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            // On remplit les tableaux ligne par ligne
+            fillTabs(line, &tab1, &tab2);
+        }
+    }
+    file.close();
+    // On tri nos tableaux
+    sort(tab1.begin(), tab1.end(), lSort);
+    sort(tab2.begin(), tab2.end(), lSort);
+
+    for (size_t i = 0; i < tab1.size(); ++i)
+    {
+        result += abs(tab1[i] - tab2[i]);
+    }
+
+    // On print le résultat
+    cout << result << endl;
+    return result;
+}
